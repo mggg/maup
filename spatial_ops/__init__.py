6b91ee9fa487f11ff2c07e332bd6873df3e0457a
Max Hully <max.hully@gmail.com>
2019-02-18 11:42:41 -0500

    Make intersections a top-level import

--- spatial_ops/__init__.py
+++ spatial_ops/__init__.py
@@ -1,5 +1,5 @@
 from .assign import assign
 from .indexed_geometries import IndexedGeometries
-from .intersection_matrix import InterpolationMatrix
+from .intersection_matrix import InterpolationMatrix, intersections
 
-__all__ = ["assign", "IndexedGeometries", "InterpolationMatrix"]
+__all__ = ["assign", "IndexedGeometries", "InterpolationMatrix", "intersections"]
