d848c9abb29f435c3317b0c1831ffd7404682a49
Max Hully <max.hully@gmail.com>
2019-06-05 14:51:49 -0400

    Add close_gaps to public API

--- maup/__init__.py
+++ maup/__init__.py
@@ -1,7 +1,15 @@
+from .adjacencies import adjacencies
 from .assign import assign
 from .indexed_geometries import IndexedGeometries
 from .intersections import intersections, prorate
-from .adjacencies import adjacencies
+from .repair import close_gaps
 
 __version__ = "0.4"
-__all__ = ["assign", "IndexedGeometries", "intersections", "prorate", "adjacencies"]
+__all__ = [
+    "assign",
+    "IndexedGeometries",
+    "intersections",
+    "prorate",
+    "adjacencies",
+    "close_gaps",
+]
