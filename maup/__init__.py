a25451ced5ebe66b49d28b92d5430300195d304e
Max Hully <max.hully@gmail.com>
2019-06-06 16:21:16 -0400

    Include resolve_overlaps in public API

--- maup/__init__.py
+++ maup/__init__.py
@@ -2,14 +2,15 @@ from .adjacencies import adjacencies
 from .assign import assign
 from .indexed_geometries import IndexedGeometries
 from .intersections import intersections, prorate
-from .repair import close_gaps
+from .repair import close_gaps, resolve_overlaps
 
 __version__ = "0.5"
 __all__ = [
     "assign",
-    "IndexedGeometries",
     "intersections",
     "prorate",
     "adjacencies",
     "close_gaps",
+    "resolve_overlaps",
+    "IndexedGeometries",
 ]
