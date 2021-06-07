f1aa829c38ec415d15409435b886f87ee4d77376
Francis Tseng <f+accounts@frnsys.com>
2021-06-07 15:45:35 -0400

    Fix IndexedGeometries for Shapely==1.7.1, see #29

--- maup/indexed_geometries.py
+++ maup/indexed_geometries.py
@@ -1,23 +1,28 @@
 import pandas
 from shapely.prepared import prep
 from shapely.strtree import STRtree
+from shapely.geometry import mapping
 from .progress_bar import progress
 
 
 def get_geometries(geometries):
     return getattr(geometries, "geometry", geometries)
 
+def hash_geom(geom):
+    return hash(tuple(mapping(geom)['coordinates']))
+
 
 class IndexedGeometries:
     def __init__(self, geometries):
         self.geometries = get_geometries(geometries)
+        self._index = {}
         for i, geometry in self.geometries.items():
-            geometry.index = i
+            self._index[hash_geom(geometry)] = i
         self.spatial_index = STRtree(self.geometries)
         self.index = self.geometries.index
 
     def query(self, geometry):
-        relevant_indices = [geom.index for geom in self.spatial_index.query(geometry)]
+        relevant_indices = [self._index[hash_geom(geom)] for geom in self.spatial_index.query(geometry)]
         relevant_geometries = self.geometries.loc[relevant_indices]
         return relevant_geometries
 
@@ -47,3 +52,4 @@ class IndexedGeometries:
         for i, target in progress(target_geometries.items(), len(target_geometries)):
             for j, intersection in self.intersections(target).items():
                 yield i, j, intersection
+
