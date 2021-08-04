24ce699d592b359314e38e1bf0a582df8cdddc60
Max Fan <root@max.fan>
2021-08-04 12:31:30 -0400

    Version bump

--- maup/__init__.py
+++ maup/__init__.py
@@ -2,11 +2,11 @@ from .adjacencies import adjacencies
 from .assign import assign
 from .indexed_geometries import IndexedGeometries
 from .intersections import intersections, prorate
-from .repair import close_gaps, resolve_overlaps, make_valid, autorepair, snap_to_grid, crop_to, doctor
+from .repair import close_gaps, resolve_overlaps, make_valid, autorepair, snap_to_grid, crop_to, expand_to, doctor
 from .normalize import normalize
 from .progress_bar import progress
 
-__version__ = "0.6"
+__version__ = "1.0.3"
 __all__ = [
     "assign",
     "intersections",
