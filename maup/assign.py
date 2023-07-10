00b0b73c0d1f9bc45a00268950c027d3c8db8869
Hazel Brenner <hnb29@cornell.edu>
2023-07-09 20:36:50 -0400

    Added rudimentary report for the assign operation. Resulting Report object knows: - source geometries (GeoDataFrame) - target geometries (GeoDataFrame) - fully covered source geometries (GeoDataFrame) -  intersections between sources and targets not accounted for by assign to max (GeoDataFrame)

--- maup/assign.py
+++ maup/assign.py
@@ -1,16 +1,19 @@
 import pandas
-
+from .report import Report
 from .indexed_geometries import IndexedGeometries
 from .intersections import intersections
 from .crs import require_same_crs
 
 
 @require_same_crs
-def assign(sources, targets):
+def assign(sources, targets, return_report=False):
     """Assign source geometries to targets. A source is assigned to the
     target that covers it, or, if no target covers the entire source, the
     target that covers the most of its area.
     """
+    assign_report = None
+    if return_report: assign_report = Report.new_assign_report(sources, targets)
+    
     assignment = pandas.Series(
         assign_by_covering(sources, targets),
         dtype="float"
@@ -18,14 +21,19 @@ def assign(sources, targets):
     assignment.name = None
     unassigned = sources[assignment.isna()]
 
+    if return_report: assign_report.fully_covered_sources = sources[~assignment.isna()]
+
     if len(unassigned):  # skip if done
         assignments_by_area = pandas.Series(
-            assign_by_area(unassigned, targets),
+            assign_by_area(unassigned, targets, assign_report),
             dtype="float"
         )
         assignment.update(assignments_by_area)
 
-    return assignment.astype(targets.index.dtype, errors="ignore")
+    if not return_report:
+        return assignment.astype(targets.index.dtype, errors="ignore")
+    else:
+        return (assignment.astype(targets.index.dtype, errors="ignore"), assign_report)
 
 
 def assign_by_covering(sources, targets):
@@ -33,8 +41,12 @@ def assign_by_covering(sources, targets):
     return indexed_sources.assign(targets)
 
 
-def assign_by_area(sources, targets):
-    return assign_to_max(intersections(sources, targets, area_cutoff=0).area)
+def assign_by_area(sources, targets, report=None):
+    pieces = intersections(sources, targets, area_cutoff=0)
+    max_area_idx = assign_to_max(pieces.area)
+
+    if report is not None: report.assign_to_max_error(pieces, max_area_idx)
+    return max_area_idx
 
 
 def assign_to_max(weights):
--- maup/report.py
+++ maup/report.py
@@ -2,6 +2,11 @@ class Report:
     def __init__(self) -> None:
         pass
 
+    def assign_to_max_error(self, pieces, max_area_idx):
+        listy_idx = list(zip(max_area_idx.index, max_area_idx))
+        self.discarded_pieces = pieces.drop(index=listy_idx)
+        
+
     @classmethod
     def new_assign_report(cls, sources, targets):
         rprt = cls()
